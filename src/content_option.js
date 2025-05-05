aba8bff628f42b17e3f777a8904a2dcd8b0b1aaf
Temiloluwa Ogundiran <temiloluuu@gmail.com>
2025-05-05 15:28:20 +0100

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,148 @@
+const logotext = "Desmond";
+const meta = {
+    title: "Desmond Dickson",
+    description: "I’m Desmond, an Email Marketing, CRM & Automation Expert",
+};
+
+const introdata = {
+    title: "I’m Desmond Dickson Oghogome",
+    animated: {
+        first: "Smart words. Smarter systems. Faster growth.",
+        second: "I write copy and build funnels that turn clicks into customers",
+        third: "I develop mobile Expert with CRMs and email tools like HubSpot, Klaviyo, and more"
+
+    },
+    description: "My goal is simple: get you more leads, more sales, and better customer loyalty — without wasting time. If you want clear marketing that feels personal and drives real growth, I’m ready to help.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About My Self",
+    aboutme: "I help businesses grow faster with smart words and smart systems. I specialize in copywriting, email marketing, automation, and building funnels that turn clicks into customers. I’m skilled with CRMs and email tools like HubSpot, Klaviyo, Mailchimp, Zoho, Apollo, Brevo, and AutomateWoo.",
+};
+
+const skills = [{
+        name: "Email Marketing",
+        value: 90,
+    },
+    {
+        name: "Product Management",
+        value: 85,
+    },
+    {
+        name: "Automation",
+        value: 80,
+    },
+    {
+        name: "Technical Writing",
+        value: 70,
+    },
+    {
+        name: "Copywriting",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Product Discovery & Design",
+        description: "●	 Wireframing, Prototyping, and Design Thinking●	 User-Centric Product Development●	 User Feedback & Iterative Improvements",
+    },
+    {
+        title: "Growth & Marketing Strategy",
+        description: "●	 Conversion Rate Optimization (CRO)●	 A/B Testing and Experimentation●	 Email Marketing and Growth Strategy●	 Content Strategy & Technical Writing"
+    },
+    {
+        title: "Product Execution & Delivery",
+        description: "● Product Roadmapping & Prioritization\n● Data-Driven Decision Making\n● Agile & Scrum (Jira, Asana, Trello)"
+    },
+];
+
+const dataportfolio = [{
+        img: "https://drive.google.com/file/d/1l-abHOKi5E-5RgovcGxlGGHVDojQy28I/view",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "desmonddickson767@gmail.com",
+    YOUR_FONE: "",
+    description: "",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    linkedin: "https://www.linkedin.com/in/desmond-dickson-oghoghome-bb5bb2178",
+    twitter: "https://x.com/desmonddickso16?s=21",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
